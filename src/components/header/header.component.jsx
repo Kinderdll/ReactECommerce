688d305e98d3aa316a371c0e2b7dea5aadf0d314
eyth_dell <eyth_dellis@hotmail.com>
2019-11-11 00:34:58 +0200

    added cartIcon and CartDropdown and implemented cartreducer and show hide functionality

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,11 +3,13 @@ import {Link} from 'react-router-dom';
 import './header.component.scss';
 import { auth } from '../../firebase/firebase.utils';
 import {connect} from 'react-redux';
-
-
-
+import CartIcon from '../cart-icon/cart-icon.component'
 import {ReactComponent as Logo} from '../../assets/crown.svg'
-const Header=({currentUser})=>(
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
+
+
+const Header=({currentUser,hidden})=>(
     <div className='header'>
         <Link className="logo-container" to="/">
             <Logo className='logo'/>
@@ -24,13 +26,18 @@ const Header=({currentUser})=>(
                 <Link className='option' to='/signin'>SIGN IN</Link>
                 )
             }
+            <CartIcon/>
         </div>
+        {hidden? null:
+        <CartDropdown/>
+        }
     </div>
 );
 
 //state here refers to rootReducer
-const mapStatetoProps= (state)=>({
-    currentUser :state.user.currentUser
+const mapStatetoProps= ({user:{currentUser},cart:{hidden}})=>({
+    currentUser :currentUser,
+    hidden:hidden
 })
 
 export default connect(mapStatetoProps,)(Header);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 
 import useReducer from './user/userReducer';
+import cartReducer from './cart/cart.reducer';
 
 
 export default combineReducers({
-    user:useReducer
+    user:useReducer,
+    cart:cartReducer
 })
