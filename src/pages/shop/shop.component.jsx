0d9227ba1f6d0e21dfd4015cf944a4e6d9ae61f7
eyth_dell <eyth_dellis@hotmail.com>
2019-11-18 22:35:00 +0200

    created collection page  , and connected that to redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import CollectionsOverviewComponents from '../../components/collections-overview/collections-overview.components'
+import {Route} from 'react-router-dom'
+import CollectionPage from '../collection/collection.componets'
 
 
 
 
-const ShopPage =({collections})=>(
+const ShopPage =({match})=>(
     <div className='shop-page'>
-        <CollectionsOverviewComponents/>
+        <Route exact path={`${match.path}`} component={CollectionsOverviewComponents}/>
+        <Route exact path={`${match.path}/:collectionId`} component={CollectionPage}/>
     </div>
 )
 
